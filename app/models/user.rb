18254f35cc1a5b460d8be2b894b66af9af23a141
george kosmopoulos <gkosmo1@hotmail.com>
2021-02-22 10:09:38 +0100

    init commit

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :flats
+
+
+  def downcase_nickname
+    nickname.downcase
+  end
+end
